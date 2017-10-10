397e738b95961fd6fa04ccbe55d1454d94e38666
vsmithers1087 <vsmithers1087@gmail.com>
2017-10-10 18:37:49 -0400

    Initial Commit

---
+++ VSVerticalSlider/VSVerticalSlider.h
@@ -0,0 +1,19 @@
+//
+//  VSVerticalSlider.h
+//  VSVerticalSlider
+//
+//  Created by Vincent Smithers on 10/10/17.
+//  Copyright © 2017 Vincent Smithers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VSVerticalSlider.
+FOUNDATION_EXPORT double VSVerticalSliderVersionNumber;
+
+//! Project version string for VSVerticalSlider.
+FOUNDATION_EXPORT const unsigned char VSVerticalSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VSVerticalSlider/PublicHeader.h>
+
+
