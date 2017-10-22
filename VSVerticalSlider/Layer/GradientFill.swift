56813773f01d20ec1b941c2387356aef924dd814
vsmithers1087 <vsmithers1087@gmail.com>
2017-10-22 03:43:21 +0200

    set marker gradient

--- VSVerticalSlider/Layer/GradientFill.swift
+++ VSVerticalSlider/Layer/GradientFill.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
+//1 init with color
+//2 sample project
+//3 readme
+
 class GradientFill: CALayer {
-    
+
     var positiveGradient = CAGradientLayer()
     var negativeGradient = CAGradientLayer()
     var positiveStartPoint = CGPoint()
     var negativeStartPoint = CGPoint()
     var positiveEndPoint = CGPoint()
     var negativeEndPoint = CGPoint()
-    var postiveColors = [UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.white.cgColor, UIColor.clear.cgColor,  UIColor.white.cgColor]
-    var negativeColors = [UIColor.orange.cgColor, UIColor.white.cgColor, UIColor.clear.cgColor, UIColor.white.cgColor, UIColor.orange.cgColor]
+    var postiveColors = [UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor]
+    var negativeColors = [UIColor.orange.cgColor, UIColor.white.cgColor, UIColor.orange.cgColor, UIColor.white.cgColor, UIColor.orange.cgColor, UIColor.white.cgColor, UIColor.orange.cgColor, UIColor.white.cgColor, UIColor.orange.cgColor, UIColor.white.cgColor, UIColor.orange.cgColor, UIColor.white.cgColor]
     var currentPoint: CGFloat!
     
     override init() {
@@ -28,29 +32,25 @@ class GradientFill: CALayer {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupBackgroundGradient()->CAGradientLayer {
-        let backgroundGradient = CAGradientLayer()
-        backgroundGradient.frame = frame
-        backgroundGradient.startPoint = CGPoint(x: 0.9, y: 0.1)
-        backgroundGradient.endPoint = CGPoint(x: 0.9, y: 0.9)
-        backgroundGradient.colors = [UIColor.clear]
-//        backgroundGradient.colors = [UIColor.purple.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.green.cgColor, UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.purple.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.green.cgColor, UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.purple.cgColor, UIColor.blue.cgColor, UIColor.cyan.cgColor, UIColor.green.cgColor, UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor]
-        return backgroundGradient
-    }
-    
-    func setup(withFrame frame: CGRect){
+    func setup(withFrame frame: CGRect) {
         self.frame = frame
-        backgroundColor = UIColor.white.cgColor
         currentPoint = frame.height / 2
         negativeGradient.colors = negativeColors
         positiveGradient.colors = postiveColors
         drawGradients(forCurrentPoint: currentPoint)
+        setBackgroundGradient()
         addSublayer(positiveGradient)
         addSublayer(negativeGradient)
-        insertSublayer(setupBackgroundGradient(), below: positiveGradient)
     }
     
-    func drawGradients(forCurrentPoint currentPoint: CGFloat){
+    func setBackgroundGradient() {
+        let backgroundGrandient = CAGradientLayer()
+        backgroundGrandient.colors = [UIColor.yellow.cgColor, UIColor.purple.cgColor, UIColor.yellow.cgColor, UIColor.purple.cgColor, UIColor.yellow.cgColor, UIColor.purple.cgColor]
+        backgroundGrandient.frame = frame
+        addSublayer(backgroundGrandient)
+    }
+    
+    func drawGradients(forCurrentPoint currentPoint: CGFloat) {
         self.currentPoint = currentPoint
         setStartingPoints()
         setEndingPoints()
