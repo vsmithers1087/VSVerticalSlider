104e667d1a77e1ec11948c6cf10bbbed0b1d154d
vsmithers1087 <vsmithers1087@gmail.com>
2017-11-11 11:40:27 -0500

    update readme images

--- VSVerticalSliderPlayground.playground/Contents.swift
+++ VSVerticalSliderPlayground.playground/Contents.swift
@@ -5,10 +5,11 @@ import VSVerticalSlider
 import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
-let color = UIColor.init(red: 50 / 255, green: 200 / 255, blue: 250 / 255, alpha: 1.0)
-let containerView = UIView(frame: CGRect(x: 20, y: 20, width: 300, height: 500))
+let color = UIColor.init(red: 0 / 255, green: 50 / 255, blue: 150 / 255, alpha: 1.0)
+let containerView = UIView(frame: CGRect(x: 20, y: 20, width: 150, height: 500))
+let compliment = UIColor.getAnalagous(color: color)
 containerView.backgroundColor = UIColor.white
-let slider = VerticalSlider(height: 400.0, primaryColor: color, offsetX: 120, offsetY: 20)
+let slider = VerticalSlider(height: 400.0, primaryColor: color, offsetX: 55, offsetY: 20)
 containerView.addSubview(slider)
 PlaygroundPage.current.liveView = containerView
 
